d30303270b5c913b384319d97214e307b3eae87e
Derek Keats <derek@dkeats.com>
2013-04-27 15:04:57 +0000

    Added code for testing Ajax pattern, the Cross-Domain Proxy, with simpleblog

--- ajaxproxy/proxy.php
+++ ajaxproxy/proxy.php
@@ -2,18 +2,36 @@
 /*
  * Code for use in cross domain Ajax with Chisimba
  */
-define('HOST', 'http://developer.thumbzup.com');
-define('PATH', '/');
+define('HOST', 'http://localhost');
+define('PATH', '/ch/');
 define('INDEX', 'index.php');
 
 $path = HOST . PATH . INDEX;
 
-$module = 'simpleblog';
+$module = $_REQUEST['module'];
+$action = $_REQUEST['action'];
+if ($action) {
+    switch ($action) {
+    case 'ajaxById':
+        $id = $_REQUEST['id'];
+        $path .= '?module=' . $module . '&action=ajaxById&id=' . $id;
+        break;
+    case 'ajaxByTag':
+        $tag = $_REQUEST['tag'];
+        $path .= '?module=' . $module . '&action=ajaxByTag&tag=' . $tag;
+        break;
+    }
+}
 
-$path .= '?module=' . $module;
+
+//$path .= '?module=' . $module . '&action=ajaxByTag&by=tag&tag=ipsum';
 
 // Open the path to read its content
 $handle = fopen($path, "r");
+
+$module = urldecode($module);
+
+//echo '<h1>' . $path . '</h1>';
 
 // If the fopen was successful, read and return it.
 if ($handle) {
